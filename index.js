9615387814d26fca82d15eafad261f5df32eb050
veenquu <41349444+veenquu@users.noreply.github.com>
2018-11-01 23:03:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ bot.on("message", function(message) {
             break;
         case "embed":
             var embed = new Discord.RichEmbed()
-                .setTitle("🔔 **Ważne ogłoszenie !**")
+                .setTitle("📋 **Regulamin Discorda !**")
+                .setFooter("Regulamin może być edytowany bez powiadomienia o tym członków Discorda.")
                 .setDescription("**1.** Nie postuj rzeczy NSFW poza kanalami NSFW \n **2.** Zakaz reklamowania innych discordów stron www itp. \n **3.** Zakaz wyzywania użytkowników oraz administracji. \n **4.** Zakaz wklejania podejrzanych linków lub reflinków. \n **5.** Earrape, krzyczenie, skakanie po kanałach głosowych jest zabronione \n **6.** Gdy widzisz że ktoś łamie regulamin to oznacz @ADMIN ")
                 .setColor(0xff0000);
             message.channel.sendEmbed(embed);
