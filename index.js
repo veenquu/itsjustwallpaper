0c3003cd3ba273541a66e9972a47b97228513d51
veenquu <41349444+veenquu@users.noreply.github.com>
2019-01-05 20:43:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,31 +27,12 @@ bot.on("message", function(message) {
             message.channel.sendMessage("Wszystkie moje komendy! \n Już niedługo !**");
             break;
         case "weryfikacja":
-    	let channel = message.channel;
-
-    // next create rich embeds
-    	let embed1 = new Discord.RichEmbed({
-        	title: 'embed1',
-        	description: 'description1',
-        	author: {
-            	name: 'author1'
-        	}
-    	});
-
-    	let embed2 = new Discord.RichEmbed({
-        	title: 'embed2',
-        	description: 'description2',
-        	author: {
-            	name: 'author2'
-        	}
-    	});
-
-    // send embed to channel
-    	channel.send(embed1)
-    	.then(msg => {
-        // after the first is sent, send the 2nd (makes sure it's in the correct order)
-        	channel.send(embed2);
-            break;
+            const embed = new Discord.RichEmbed()
+                .setTitle("**Weryfikacja**")
+                .setColor(0x00CED1)
+                .setDescription("Niedługo ktoś z liderówki, zaakceptuje twoje zgłoszenie ! <@&527604678314426424>")
+                message.channel.send({embed});
+                break;
         case "xyz":
              const secondembed = new Discord.RichEmbed()
                   .setTitle ("fewfewf")
