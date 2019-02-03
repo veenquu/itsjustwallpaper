1ae604a71158c2500261b238cd3f412d7c105db0
veenquu <41349444+veenquu@users.noreply.github.com>
2019-02-03 22:35:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,20 @@ bot.on("message", function(message) {
         case "pomoc":
             message.channel.sendMessage("Wszystkie moje komendy! \n Już niedługo !**");
             break;
-        case "xxxx":
+        case "regulamin":
             const embed = new Discord.RichEmbed()
                 .setTitle("📋 **Regulamin**")
                 .setColor(0x00CED1)
                 .setDescription("**1.** Zachowanie: \n **1.1** Musisz być uprzejmy dla klienta i dla kolegów z pracy \n **2.** Wypłaty: \n **2.1** Aby otrzymać wypłatę muszisz conajmniej spędzić 30 minut na duty. \n **30 minut:** 200$ \n **60 minut** 500$ \n **Akcja:** Gwarantowane 250$ \n **3.** Reklamowanie: \n **3.1** Nie możesz reklamować discordów lub stron www bez pozwolenia.")
                 .setFooter("Regulamin może się w każdej chwili zmienić")
                 message.channel.send({embed});
+                break;
+        case "xxxx":
+            const embed = new Discord.RichEmbed()
+                .setTitle("XX")
+                .setColor(0x00CED1)
+                .setDescription("XDXD")
+                message.channel.send({otherEmbed});
                 break;
         default:
             message.channel.sendMessage("Nieprawidlowa komenda!")
